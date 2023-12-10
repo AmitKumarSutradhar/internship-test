51fd06ae3a81cf4bd3c74edd5a8d10786ae4dd4f
Amit Kumar Sutradhar <amit596450@gmail.com>
2023-12-10 17:17:37 +0600

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,42 @@
-<h1>Instructions</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        header{
+            background-color: #3800AF;
+        }
+        main{
+            display: grid;
+            grid-template-columns: 25% auto;
+            background-color: #3800AF;
+        }
+        main .sidebar{
+            display: grid;
+            background-color: #3800AF;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h2 class="text-center text-white my-3 p-4">Start Time: 4:30 & End Time: 5:30</h2>
+    </header>
 
+    <main>
+        <div class="sidebar">
+            <p class="text-white p-4">Hi. I am Amit Kumar Sutradhar. I have completed my graduation in Computer Science Engineering. I have always been interested in web development and have done several projects based Laravel Framework. I have a passion for learning and sharing my knowledge with others as publicly as possible. I love to solve real-world problems. I am strategic and goal-oriented and always work with an end goal in mind.</p>
+        </div>
+        <div class="section">
+            <h2>Hello</h2>
+        </div>
+    </main>
 
-<p>
-    -- follow the instructions Readme.Md file and create this view here and render the data list.
-</p>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+</html>
